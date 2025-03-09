60b5620180f9cb0ae2938f13730e8d6cdf7f2efa
Arman <armanar6712@gmail.com>
2025-03-09 17:04:42 +0530

    small changes

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,13 @@
 import { Header } from "./Header";
 import { Footer } from "./Footer";
 import { Hero } from "./Hero";
-import { ListItem } from "./ListItem";
-import { ThemeToggle } from "./ThemeToggle";
+import { ListItem } from "./ui/ListItem";
+import { ThemeToggle } from "./ui/ThemeToggle";
 
-import { BaseContainer } from "./BaseContainer";
-import { BaseButton } from "./BaseButton";
+import { BaseContainer } from "./ui/BaseContainer";
+import { BaseButton } from "./ui/BaseButton";
+
+import { Logo } from "./ui/Logo";
 
 export {
   Header,
@@ -15,4 +17,5 @@ export {
   ThemeToggle,
   BaseContainer,
   BaseButton,
+  Logo,
 };
