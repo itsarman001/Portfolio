3363cbcdde99e34b0b18a5ec96d6568c46d65b23
Md Arman <armanar6712@gmail.com>
2025-04-18 23:20:42 +0530

    Fix: Logo Color

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -2,7 +2,7 @@ export const Logo = ({ onClick }) => {
   return (
     <h1
       onClick={onClick}
-      className="tracking-tighter font-bold text-xl cursor-pointer"
+      className="tracking-tighter font-bold text-xl text-primary/90 cursor-pointer"
     >
       Arman
     </h1>
