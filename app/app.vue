f3917cc84e4fb572bcfd5c06521bfee890789c0f
Md Arman <armanmd6712@gmail.com>
2026-01-11 19:01:31 +0530

    Migration Clearnup and Folder Strutcure

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="flex items-center justify-center h-screen w-screen">
+    <h1 class="text-6xl font-bold">Portfolio Migration React.js to Nuxt.js</h1>
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/image']
-})
+
+  modules: [
+    "@nuxt/eslint",
+    "@nuxtjs/tailwindcss",
+    "@nuxt/hints",
+    "@nuxt/icon",
+    "@nuxt/image",
+  ],
+});
