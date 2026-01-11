c1f87a64404cb678ae723c84da5130e8019bad28
Md Arman <armanmd6712@gmail.com>
2026-01-11 19:58:10 +0530

    Add Layout

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="flex items-center justify-center h-screen w-screen">
-    <h1 class="text-6xl font-bold">Portfolio Migration React.js to Nuxt.js</h1>
-  </div>
+  <!-- بِسْمِ ٱللَّهِ ٱلرَّحْمَٰنِ ٱلرَّحِيْمِ -->
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
